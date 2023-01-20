6796241366c1136a08ca4fad26f2f34e7d848ab9
Shashank Shree Neupane <shashank@neupane.com.np>
2023-01-20 23:59:53 +0545

    Update challenge-1.js

--- challenge-1.js
+++ challenge-1.js
@@ -1,1 +1,64 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'findDigits' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function findDigits(n) {
+    // Write your code here
+    var newNumber = n;
+    let count = 0;
+    var digits = n.toString().split('');
+    var realDigits = digits.map(Number);
+    //let uniqueNos = realDigits.filter((item, i, ar) => ar.indexOf(item) === i);
+    //realDigits.sort(function(a, b) {  return a - b; });
+    for(let i=0;i<realDigits.length;i++){
+        if(newNumber % realDigits[i]==0){
+            count++;
+        }
+    }
+    return count;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const n = parseInt(readLine().trim(), 10);
+
+        const result = findDigits(n);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
 
